c208c355209618708e091f91c5295e31ba707e85
Nico-010 <nicolaschalatashvili@outlook.com>
2023-12-13 17:11:53 +0400

    .

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: ["cypress/e2e/2-advanced-examples"],
   },
 });
